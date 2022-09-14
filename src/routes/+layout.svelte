11a71259ae3a7a44828a2bd122798f96d4bee63d
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-09-14 14:32:16 +0200

    Update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,14 +15,16 @@
 	<slot />
 </PageTransition>
 
-<footer class="py-6 bg-nord1 overflow-hidden max-w-full">
+<footer class="bg-nord1 overflow-hidden max-w-full py-4">
 	<div class="container mx-auto text-center">
 		<div class="flex flex-col">
 			{@html data.joke}
 		</div>
 
-		<p class="text-xs">
-			Not funny? <button on:click={fetchJoke}>[try again!]</button>
-		</p>
+		<hr class="border-nord2" />
+
+		<div class="text-xs mt-4">
+			Not funny? - <button class="text-nord10" on:click={fetchJoke}>[try again]</button>
+		</div>
 	</div>
 </footer>
