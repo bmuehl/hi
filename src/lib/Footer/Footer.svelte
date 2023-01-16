0681825749870549b7a2681f984d77ef2e06cbbb
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2023-01-16 16:17:16 +0100

    Change margins

--- src/lib/Footer/Footer.svelte
+++ src/lib/Footer/Footer.svelte
@@ -9,13 +9,13 @@
 
 <footer class="bg-nord1 overflow-hidden w-full py-2 shadow-lg">
 	<div class="container mx-auto text-center flex flex-col px-2">
-		<div class="text-xs mb-2 flex flex-col">
+		<div class="text-xs flex flex-col">
 			{@html joke}
 		</div>
 
-		<hr class="border-nord2" />
+		<hr class="border-nord2 my-3" />
 
-		<div class="text-xs mt-2">
+		<div class="text-xs mb-2">
 			<button class="text-nord10" on:click={fetchJoke}>[try again]</button>
 		</div>
 	</div>
