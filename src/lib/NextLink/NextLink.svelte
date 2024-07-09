e4353806e2dd113102b674709571c749acd0dfc2
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-07-09 14:55:22 +0200

    disable on dbltap for mobile

--- src/lib/NextLink/NextLink.svelte
+++ src/lib/NextLink/NextLink.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import Kbd from '$lib/Kbd/Kbd.svelte';
 	import Icon from '$lib/Icon/Icon.svelte';
 	import DoubleTab from '$lib/Icon/icons/DoubleTab.svelte';
 	import { clickOnKey } from '$lib/utils';
-	import { onDoubleTap } from '$lib/utils';
 
 	export let href: string;
 	export let top = false;
@@ -15,13 +13,11 @@
 	use:clickOnKey={[' ']}
 	data-sveltekit-preload-code="viewport"
 	{href}
-	use:onDoubleTap
-	ondbltap={() => goto(href)}
 >
 	<span>{top ? 'to start' : 'next page'}</span>
 </a>
 <div class="w-2"></div>
 <span class="hidden md:inline-flex md:items-center"><Kbd>Space</Kbd></span>
-<span class="inline-flex items-center md:hidden">
+<span class="hidden items-center">
 	<Kbd icon><Icon src={DoubleTab} size="md" /></Kbd>
 </span>
